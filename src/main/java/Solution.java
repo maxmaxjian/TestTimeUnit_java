dfea1b7ce566d882f7683d07419b7eea4e89b8aa
Jian Wei <wejian@amazon.com>
2019-08-14 19:18:43 -0400

    first submission

---
+++ src/main/java/Solution.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.TimeUnit;
+
+public class Solution {
+    public static void main(String[] args) {
+        long time_out = TimeUnit.SECONDS.toSeconds(10);
+        System.out.println("time_out = " + time_out);
+    }
+}
